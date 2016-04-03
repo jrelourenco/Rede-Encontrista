b45b0fed2e45971c942b72034c4d14b3fcd8324e
Mário Santos <mariosantos77@gmail.com>
2016-04-03 11:42:47 +0100

    Small Login Refactor

--- login.php
+++ login.php
@@ -24,12 +24,14 @@
             <span class="input-group-addon"><i class="fa fa-user"></i></span>
             <input type="text" class="form-control" placeholder="Email">
         </div>
+
         <!-- Password Field -->
         <div class="input-group input-group-lg">
             <span class="input-group-addon"><i class="fa fa-lock"></i></span>
             <input type="password" class="form-control" placeholder="Password">
         </div>
 
+        <!-- Login Button -->
         <button type="submit" class="float">Login</button>
         </form>
         <a href="recover.php">Recuperar Password</a>
