c7b59dac716a3fb3224b4913db68cbd428da4cea
Mário Santos <mariosantos77@gmail.com>
2016-04-03 14:24:40 +0100

    Changed page description.

--- html-head.php
+++ html-head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <meta name="description" content="Blueprint: Slide and Push Menus" />
+    <meta name="description" content="Rede Encontristas - MEJSh" />
     <meta name="keywords" content="shalom rede cursos" />
     <meta name="author" content="Codrops" />
     <link rel="shortcut icon" href="laravel/public/images/favicon.png">
