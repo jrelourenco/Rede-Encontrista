458492ddd2f5af51c9612100e3c9748136965ea4
jrel <jrelo@iscte.pt>
2016-07-01 22:05:38 +0100

    Login blade criado

--- laravel/resources/views/login.blade.php
+++ laravel/resources/views/login.blade.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="pt">
-<?php include 'html-head.php'; ?>
-<body>
+@extends('layout.master')
+@section('title')
+Rede Encontristaas: Perfil
+@stop
+@section('main')
     <div class="login">
     <div class="heading">
     <h2>Rede Encontristas</h2>
     <h3>Bem vindo! Insere as credenciais para entrares</h3>
-        <form action="index.php" method="post">
+        <form action="../../../index.php" method="post">
         <!-- Username Field -->
         <div class="input-group input-group-lg">
             <span class="input-group-addon"><i class="fa fa-user"></i></span>
@@ -25,5 +26,4 @@
         <a href="recover.php">Recuperar Password</a>
     </div>
     </div>
-</body>
-</html>
+@stop
