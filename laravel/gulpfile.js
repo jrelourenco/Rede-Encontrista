2437d5dd921df08574e05ef5601f5168a6facb42
jrel <jrelo@iscte.pt>
2016-07-12 13:42:12 +0100

    correção do gulp para importar os icons

--- laravel/gulpfile.js
+++ laravel/gulpfile.js
@@ -11,6 +11,6 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+elixir(function (mix) {
+    mix.sass('app.scss').copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/', 'public/fonts');
 });
