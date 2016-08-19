8638b75b64e187a27814c181a4015c700a9a01e2
jrel <jrelo@iscte.pt>
2016-08-19 16:20:37 +0100

    limpesa do projeto

--- laravel/gulpfile.js
+++ laravel/gulpfile.js
@@ -12,20 +12,26 @@ var elixir = require('laravel-elixir');
  */
 
 jsPublicPath = 'resources/assets/js/public';
+cssPublicPath = 'resources/assets/css';
+fontPublicPath = 'public/fonts/';
 elixir.config.sourcemaps = false;
 
 elixir(function (mix) {
+   
     mix.sass('app.scss');
-
-    mix.copy('node_modules/jquery/dist/jquery.js', jsPublicPath);
+    
+    mix.copy('node_modules/jquery/jquery.js', jsPublicPath);
     mix.copy('node_modules/bootstrap-sass/assets/javascripts/bootstrap.js', jsPublicPath);
     mix.copy('node_modules/angular/angular.js', jsPublicPath);
 
+
+    mix.copy('node_modules/font-awesome/fonts', fontPublicPath);
 
     mix.scripts([
         'public/jquery.js',
         'public/bootstrap.js',
         'public/angular.js',
-    ]);
-    mix.scriptsIn('private');
+        ]);
+    
+
 });
